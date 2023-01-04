1747d7df13cb39310a7c0b739ae7c6771d7e8cf6
RamonSandoval <102700747+RamonSandoval@users.noreply.github.com>
2023-01-03 22:10:07 -0800

    ,

--- pages/calendarMaint.js
+++ pages/calendarMaint.js
@@ -3,15 +3,13 @@ import React from "react";
 import Layout from "../components/Layout";
 import styles from "../styles/CalendarMaint.module.css";
 import CalendarM from "../components/CalendarM.js";
-import CalendarAnt from '../components/CalendarAnt'
 import { useState } from "react";
 const CalendarMaint = () => {
   return (
     <>
       <Layout tituloPagina="Calendario" />
       <Center>
-      {/*<CalendarM/>*/}
-      <CalendarAnt/>
+      <CalendarM/>
       </Center>
     </>
   );
